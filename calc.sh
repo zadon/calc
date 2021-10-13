fc37a88accb4eb0c15e35735a6c81a8878ca6d50
zadon <69627510+zadon@users.noreply.github.com>
2021-10-13 12:33:20 +0800

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -6,14 +6,12 @@ if (($# == 1)); then
   echo "$RESULT1"
 elif (($# == 2)); then
  let RESULT1=`cat $1`
-
  let RESULT2=`cat $2`
-
  if (("$RESULT1" > "$RESULT2")); then
    echo "$RESULT1"
  else
    echo "$RESULT2"
  fi
 else 
-   echo "please input one or two parameters"
+ echo "please input one or two parameters"
 fi
